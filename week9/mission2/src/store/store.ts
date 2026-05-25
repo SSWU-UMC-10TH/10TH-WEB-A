c86047884027f5110a8b86bc934f204fb2d4fe5e
kimseongyeon <seongyeon0810@naver.com>
2026-05-26 03:14:58 +0900

    feat: 9주차 미션2

---
+++ week9/mission2/src/store/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../slices/cartSlice";
+import modalReducer from '../slices/modalSlice'
+
+//1. 저장소 생성
+const store = configureStore({
+    //2. 리듀서 설정
+    reducer: {
+        cart: cartReducer,
+        modal: modalReducer,
+    },
+});
+
+// store를 활용할 수 있도록 내보내야 함
+// 여기서 실행해서 스토어를 뺴준다
+// 싱글톤패턴
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
